9c97dbc27a79d6c66679ef99c98bd50d7fbb4abe
Corentin Braud <corentin.braud95@gmail.com>
2024-01-30 00:34:10 +0100

    Home page avancée avec style global des sections faite

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom'
+import Portrait from '../assets/portrait.jpg'
 import ContactButton from '../components/ContactButton'
 import LinkButton from '../components/LinkButton'
 
 function Home() {
   return (
     <main className='home-page'>
+      <section className='title-about-wrapper'>
+        <div className='title-container'>
+          <img
+            src={Portrait}
+            alt='Portrait de Corentin Braud'
+          />
+          <h1>Développeur web</h1>
+        </div>
+        <div className='about-container'>
+          <h2>À propos de moi</h2>
+          <p>
+            Bonjour, je suis un développeur web français, vivant dans le nord de la région
+            parisienne. Passionné par le développement web et l’expérience utilisateur, j'adore
+            explorer de nouvelles technologies web et je suis toujours en quête de nouvelles
+            compétences à acquérir.
+          </p>
+          <Link className='link'>
+            En savoir plus <i className='fa-solid fa-right-to-bracket'></i>
+          </Link>
+        </div>
+      </section>
       <article className='social-contact-wrapper'>
         <div className='social-container'>
           <h2>Social</h2>
