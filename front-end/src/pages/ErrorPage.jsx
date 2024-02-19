7564a2608eef88a31867b90b28d2b1b6eeae69b5
Corentin Braud <corentin.braud95@gmail.com>
2024-02-19 01:26:49 +0100

    Page d'erreur

--- front-end/src/pages/ErrorPage.jsx
+++ front-end/src/pages/ErrorPage.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
+
 function ErrorPage() {
-  return <main>Error page</main>
+  return (
+    <main className='error-container'>
+      <p className='p-large-style error-text'>Ceci est un message d'erreur.</p>
+      <Link
+        to='/'
+        className='link p-large-style error-link'
+      >
+        Accueil <i className='fa-solid fa-right-to-bracket'></i>
+      </Link>
+    </main>
+  )
 }
 
 export default ErrorPage
